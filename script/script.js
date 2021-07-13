c0f20c7964e0a540c4ceab486481638fae190acb
aser <asero4eg@gmail.com>
2021-07-13 18:53:00 +0300

    gotop btn fix

--- script/script.js
+++ script/script.js
@@ -1,19 +1,15 @@
-function inViewport(element) {
-    var bb = element.getBoundingClientRect();  // Get the elements position relative to the viewport
-    // Check if the element is outside the viewport
-    // Then invert the returned value because you want to know the opposite
-    return !(bb.top > innerHeight || bb.bottom < 0);
-}
-var myElement = document.querySelector('.steps'),
-    gotopBtn=document.querySelector('.gotop')
-// Listen for the scroll event 
-document.addEventListener('scroll', event => {
-    // Check the viewport status 
-    if (inViewport(myElement)) {
-        gotopBtn.style.opacity = '0';
-        gotopBtn.style.right = '-60px';
-    } else {
-        gotopBtn.style.opacity = '1';
-        gotopBtn.style.right = '20px';
-    }
-})
+var btn = $('.gotop');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html').animate({scrollTop:0}, '300');
+});
+
