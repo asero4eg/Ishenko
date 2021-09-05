c69d57e2f5a82b73b5aff81562fb9c316c033aef
aser <asero4eg@gmail.com>
2021-09-05 19:12:05 +0300

    about us added

---
+++ script/script.js
@@ -0,0 +1,9 @@
+// Smooth links scroll
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+      });
+  });
+});
