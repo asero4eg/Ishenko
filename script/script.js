2ef1298b04cdc66c776e418e87f80b8c35aef44c
aser <asero4eg@gmail.com>
2021-09-05 22:09:34 +0300

    preloader added

--- script/script.js
+++ script/script.js
@@ -1,17 +1,21 @@
+setTimeout(() => {
+  let element=document.querySelector('.preloader');
+  element.parentNode.removeChild(element);
+}, 2000)
 // Smooth links scroll
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   anchor.addEventListener('click', function (e) {
-      e.preventDefault();
-      document.querySelector(this.getAttribute('href')).scrollIntoView({
-          behavior: 'smooth'
-      });
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
   });
 });
 
 // Go top btn
 var btn = $('.gotop');
 
-$(window).scroll(function() {
+$(window).scroll(function () {
   if ($(window).scrollTop() > 300) {
     btn.addClass('show');
   } else {
@@ -19,7 +23,7 @@ $(window).scroll(function() {
   }
 });
 
-btn.on('click', function(e) {
+btn.on('click', function (e) {
   e.preventDefault();
-  $('html').animate({scrollTop:0}, '300');
+  $('html').animate({ scrollTop: 0 }, '300');
 });
