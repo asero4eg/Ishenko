4ae25edf3877521fb0717c08d63cef591b6319f3
aser <asero4eg@gmail.com>
2021-09-06 21:52:21 +0300

    popup try

--- script/script.js
+++ script/script.js
@@ -1,8 +1,3 @@
-// setTimeout(() => {
-//   let element=document.querySelector('.preloader');
-//   element.parentNode.removeChild(element);
-// }, 2000)
-
 // Smooth links scroll
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   anchor.addEventListener('click', function (e) {
@@ -28,3 +23,73 @@ btn.on('click', function (e) {
   e.preventDefault();
   $('html').animate({ scrollTop: 0 }, '300');
 });
+
+//Popup documents
+let docLinks = document.querySelectorAll('.documents-link');
+let bg = document.querySelector('.documents-wrapper');
+
+
+for (let i = 0; i < docLinks.length; i++) {
+  console.log(docLinks[i].dataset.id)
+  docLinks[i].addEventListener('click', docLinkShow)
+}
+function docLinkShow(e) {
+
+  if (e.target.dataset.id === 'legalize') {
+    if (document.querySelector('#legalize-doc').classList.contains('documents-needed-hide')) {
+      bg.style.display = 'block';
+      document.querySelector('#legalize-doc').classList.remove('documents-needed-hide');
+      document.querySelector('#legalize-doc').classList.add('documents-needed-show');
+      console.log('legalize')
+    }
+    else {
+      document.querySelector('#legalize-doc').classList.add('documents-needed-hide');
+      document.querySelector('#legalize-doc').classList.remove('documents-needed-show');
+      bg.style.display = 'none';
+      console.log('legalize-hide');
+      console.log(document.querySelector('#legalize-doc').classList)
+    }
+  }
+  if (e.target.dataset.id === 'privat') {
+    if (document.querySelector('#privat-doc').classList.contains('documents-needed-hide')) {
+      bg.style.display = 'block';
+      document.querySelector('#privat-doc').classList.remove('documents-needed-hide');
+      document.querySelector('#privat-doc').classList.add('documents-needed-show');
+      console.log('privat')
+    }
+    else {
+      document.querySelector('#privat-doc').classList.add('documents-needed-hide');
+      document.querySelector('#privat-doc').classList.remove('documents-needed-show');
+      bg.style.display = 'none';
+      console.log('privat-hide')
+    }
+  }
+  if (e.target.dataset.id === 'geodezic') {
+    if (document.querySelector('#geodezic-doc').classList.contains('documents-needed-hide')) {
+      bg.style.display = 'block';
+      document.querySelector('#geodezic-doc').classList.remove('documents-needed-hide');
+      document.querySelector('#geodezic-doc').classList.add('documents-needed-show');
+      console.log('geodezic')
+    }
+    else {
+      document.querySelector('#geodezic-doc').classList.add('documents-needed-hide');
+      document.querySelector('#geodezic-doc').classList.remove('documents-needed-show');
+      bg.style.display = 'none';
+      console.log('geodezic-hide')
+    }
+  }
+  if (e.target.dataset.id === 'kadastr') {
+    if (document.querySelector('#kadastr-doc').classList.contains('documents-needed-hide')) {
+      bg.style.display = 'block';
+      document.querySelector('#kadastr-doc').classList.remove('documents-needed-hide');
+      document.querySelector('#kadastr-doc').classList.add('documents-needed-show');
+      console.log('kadastr')
+    }
+    else {
+      document.querySelector('#kadastr-doc').classList.add('documents-needed-hide');
+      document.querySelector('#kadastr-doc').classList.remove('documents-needed-show');
+      bg.style.display = 'none';
+      console.log('kadastr-hide')
+    }
+  }
+}
