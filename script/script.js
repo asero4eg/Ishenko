70eb878d8c4043fccbfbe511ecf21c6e941c9232
aser <asero4eg@gmail.com>
2021-09-07 17:04:41 +0300

    modal windows update

--- script/script.js
+++ script/script.js
@@ -24,75 +24,58 @@ btn.on('click', function (e) {
   $('html').animate({ scrollTop: 0 }, '300');
 });
 
-//Popup documents
+// Modal documents
 let docLinks = document.querySelectorAll('.documents-link');
-let bg = document.querySelector('.documents-wrapper');
-let legalize = document.querySelector('#legalize-doc');
-let privat = document.querySelector('#privat-doc');
-let geodezic = document.querySelector('#geodezic-doc');
-let kadastr = document.querySelector('#kadastr-doc');
+
+let bg = document.querySelector('#modal-bg'),
+  legalize = document.querySelector('#legalize-doc'),
+  privat = document.querySelector('#privat-doc'),
+  geodezic = document.querySelector('#geodezic-doc'),
+  kadastr = document.querySelector('#kadastr-doc');
+let modalWindows = document.querySelectorAll('.documents-needed');
 
 
 for (let i = 0; i < docLinks.length; i++) {
   console.log(docLinks[i].dataset.id)
-  docLinks[i].addEventListener('click', docLinkShow)
+  docLinks[i].addEventListener('click', docShow)
 }
-function docLinkShow(e) {
+bg.addEventListener('click', docHide)
 
-  if (e.target.dataset.id === 'legalize') {
-    if (legalize.classList.contains('documents-needed-hide')) {
-      bg.style.display = 'block';
-      legalize.classList.remove('documents-needed-hide');
-      legalize.classList.add('documents-needed-show');
-      console.log('legalize')
-    }
-    else {
-      legalize.classList.add('documents-needed-hide');
-      legalize.classList.remove('documents-needed-show');
-      bg.style.display = 'none';
-      console.log('legalize-hide');
-    }
+function docShow(e) {
+  if (e.target.dataset.id === 'legalize' && legalize.classList.contains('documents-needed-hide')) {
+    bg.style.display = 'block';
+   document.querySelector('body').style.overflowY='hidden';
+    legalize.classList.remove('documents-needed-hide');
+    legalize.classList.add('documents-needed-show');
+    console.log('legalize')
   }
-  if (e.target.dataset.id === 'privat') {
-    if (privat.classList.contains('documents-needed-hide')) {
-      bg.style.display = 'block';
-      privat.classList.remove('documents-needed-hide');
-      privat.classList.add('documents-needed-show');
-      console.log('privat')
-    }
-    else {
-      privat.classList.add('documents-needed-hide');
-      privat.classList.remove('documents-needed-show');
-      bg.style.display = 'none';
-      console.log('privat-hide')
-    }
+  if (e.target.dataset.id === 'privat' && privat.classList.contains('documents-needed-hide')) {
+    bg.style.display = 'block';
+    document.querySelector('body').style.overflowY='hidden';
+    privat.classList.remove('documents-needed-hide');
+    privat.classList.add('documents-needed-show');
+    console.log('privat')
   }
-  if (e.target.dataset.id === 'geodezic') {
-    if (geodezic.classList.contains('documents-needed-hide')) {
-      bg.style.display = 'block';
-      geodezic.classList.remove('documents-needed-hide');
-      geodezic.classList.add('documents-needed-show');
-      console.log('geodezic')
-    }
-    else {
-      geodezic.classList.add('documents-needed-hide');
-      geodezic.classList.remove('documents-needed-show');
-      bg.style.display = 'none';
-      console.log('geodezic-hide')
-    }
+  if (e.target.dataset.id === 'geodezic' && geodezic.classList.contains('documents-needed-hide')) {
+    bg.style.display = 'block';
+    document.querySelector('body').style.overflowY='hidden';
+    geodezic.classList.remove('documents-needed-hide');
+    geodezic.classList.add('documents-needed-show');
+    console.log('geodezic')
   }
-  if (e.target.dataset.id === 'kadastr') {
-    if (kadastr.classList.contains('documents-needed-hide')) {
-      bg.style.display = 'block';
-      kadastr.classList.remove('documents-needed-hide');
-      kadastr.classList.add('documents-needed-show');
-      console.log('kadastr')
-    }
-    else {
-      kadastr.classList.add('documents-needed-hide');
-      kadastr.classList.remove('documents-needed-show');
-      bg.style.display = 'none';
-      console.log('kadastr-hide')
-    }
+  if (e.target.dataset.id === 'kadastr' && kadastr.classList.contains('documents-needed-hide')) {
+    bg.style.display = 'block';
+    document.querySelector('body').style.overflowY='hidden';
+    kadastr.classList.remove('documents-needed-hide');
+    kadastr.classList.add('documents-needed-show');
+    console.log('kadastr')
+  }
+}
+function docHide() {
+  for (let i = 0; i < modalWindows.length; i++) {
+    bg.style.display = 'none';
+    document.querySelector('body').style.overflowY='auto';
+    modalWindows[i].classList.remove('documents-needed-show');
+    modalWindows[i].classList.add('documents-needed-hide');
   }
 }
