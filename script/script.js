39ddd67cb173948a17598dc2472a643ed23ea330
aser <asero4eg@gmail.com>
2021-09-15 23:37:37 +0300

    gyro test

--- script/script.js
+++ script/script.js
@@ -153,3 +153,29 @@ tippy('.footer-copyright', {
   followCursor: true,
   maxWidth: 450,
 });
+// Phone gyro rotate
+
+let counter = 0;
+const updateRate = 10;
+const limit = 45;
+const tiltable = document.querySelector(".logo-img");
+
+function updateNow() {
+  return counter++ % updateRate === 0;
+};
+
+window.addEventListener("deviceorientation", function (event) {
+  if (updateNow()) {
+    let position = Math.round(event.gamma);
+    if (Math.abs(position) > limit) {
+      if (position > limit) {
+        position = limit;
+      } else {
+        position = -limit;
+      }
+    }
+    position = position / -100;
+    let style = "rotateY(" + position + "deg)";
+    tiltable.style.transform = style;
+  }
+});
